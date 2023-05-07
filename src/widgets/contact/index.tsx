51e1755fd991abe9303100411ae8f917d0705478
hojooneum <bldolphin96@gmail.com>
2023-05-07 23:02:03 +0900

    feat(contact): implement contact

--- src/widgets/contact/index.tsx
+++ src/widgets/contact/index.tsx
@@ -1,8 +1,22 @@
 import React, { FC } from 'react';
 
+import { PhoneIcon, PlusSquareIcon } from '@chakra-ui/icons';
+import { Container, Link, List, ListIcon, ListItem } from '@chakra-ui/react';
+
 interface IContact {}
-const Contact: FC = ({}: IContact) => {
-  return <></>;
-};
+const Contact: FC<IContact> = ({}: IContact) => (
+  <Container>
+    <List>
+      <ListItem>
+        <ListIcon as={PhoneIcon} />
+        +8210-6395-9161
+      </ListItem>
+      <ListItem>
+        <ListIcon as={PlusSquareIcon} />
+        <Link href="https://github.com/minusmo">minusmo</Link>
+      </ListItem>
+    </List>
+  </Container>
+);
 
 export default Contact;
