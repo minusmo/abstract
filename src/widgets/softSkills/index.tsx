de60e03b9b3fcbe4e4f69109c44079e233016c80
hojooneum <bldolphin96@gmail.com>
2023-05-07 21:46:29 +0900

    feat(softSkills): implement softSkills

--- src/widgets/softSkills/index.tsx
+++ src/widgets/softSkills/index.tsx
@@ -1,8 +1,37 @@
 import React, { FC } from 'react';
 
-interface ISoftSkills {}
-const SoftSkills: FC = ({}: ISoftSkills) => {
-  return <></>;
-};
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Container,
+  Heading,
+  Stack,
+  StackDivider,
+} from '@chakra-ui/react';
+import Title from '../../components/title';
+
+interface ISoftSkills {
+  softSkills: string[];
+}
+const SoftSkills: FC<ISoftSkills> = ({ softSkills }: ISoftSkills) => (
+  <Container>
+    <Card>
+      <CardHeader>
+        <Heading>Soft Skills</Heading>
+      </CardHeader>
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing={3}>
+          {softSkills.map((softSkill) => (
+            <Box>
+              <Title heading={softSkill} tag={'h4'} />
+            </Box>
+          ))}
+        </Stack>
+      </CardBody>
+    </Card>
+  </Container>
+);
 
 export default SoftSkills;
